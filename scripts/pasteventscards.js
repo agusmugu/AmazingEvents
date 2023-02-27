0a3887fe65805f747faf2c0fec06232d7fad9064
agusmugu <agustinmuguruza@gmail.com>
2023-02-27 00:19:07 -0300

    cards.events

--- scripts/pasteventscards.js
+++ scripts/pasteventscards.js
@@ -187,16 +187,19 @@ let data = {
   }
 
   function createTemplate(){
-    let events= data.events
+    let events=data.events
 
     let templates=[]
 
     for(let one of events){
+        if(one.assistance){
         templates.push(template(one.image,one.name,one.description,one.price))
+        }
     }
+    console.log(templates)
     let selector=document.getElementById("card-conteiner")
 
     selector.innerHTML=templates.join("")
-  }
+    }
 
-  createTemplate()
+createTemplate()
