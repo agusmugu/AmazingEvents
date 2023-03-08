72e07c656067bca5feccc0bfbe526f00960b4626
Agusmugu <agustinmuguruza@gmail.com>
2023-03-08 16:38:31 -0300

    checks

---
+++ scripts/upcomingeventscards.js
@@ -0,0 +1,17 @@
+function createTemplate(){
+    let events=data.events
+
+    let templates=[]
+
+    for(let one of events){
+        if(one.date > data.currentDate){
+        templates.push(template(one.image,one.name,one.description,one.price))
+        }
+    }
+    console.log(templates)
+    let selector=document.getElementById("card-conteiner")
+
+    selector.innerHTML=templates.join("")
+    }
+
+createTemplate()
